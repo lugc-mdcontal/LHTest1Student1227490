05c52b757c8795216ff4b2b24d4701099f9c3d75
Matthew Daniel Contaldi <mdcontal@lakeheadu.ca>
2024-02-05 13:40:02 -0500

    question 2.4 complete

--- src/Component.java
+++ src/Component.java
@@ -1,7 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Component {
     private String Name;
     private String Manufacturer;
     private double Price;
+
+    // static because one instance, and final because we don't want to edit it later
+    static final List<String> Valid = Arrays.asList("3Com","Acer","Arctic","AMD", "Asus","Apple Inc.","Bose","Cooler Master", "Hitachi", "Intel", "Logitech", "Marvell", "Nvidia", "Qualcomm", "Samsung","Tyan","Fujitsu","MSI","Seagate","Toshiba", "Western Digital", "XTREEM");
 
     private void setName(String Name)
     {
@@ -14,9 +20,17 @@ public class Component {
     private void setPrice(double Price)
     {
         if (Price < 0 || Price > 10000)
-            throw new IllegalArgumentException("The price needs to be within 0-10000 (inclusive)");
+            throw new IllegalArgumentException("The price needs to be within 0-10000 (inclusive).");
 
         this.Price = Price;
+    }
+
+    private void setManufacturer(String Manufacturer)
+    {
+        if (!Valid.contains(Manufacturer))
+            throw new IllegalArgumentException(Manufacturer + " is not in the valid list.");
+
+        this.Manufacturer = Manufacturer;
     }
 
     public Component(String Name, String Manufacturer, double Price)
