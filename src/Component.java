b64328a76e6c42bb1ad1c9321b13e3cf50565bbd
Matthew Daniel Contaldi <mdcontal@lakeheadu.ca>
2024-02-05 13:34:03 -0500

    question 2.3 complete

--- src/Component.java
+++ src/Component.java
@@ -7,8 +7,18 @@ public class Component {
     {
         if (Name.length() < 3)
             throw new IllegalArgumentException("The name needs to be at least 3 chars long.");
+
+        this.Name = Name;
     }
-    
+
+    private void setPrice(double Price)
+    {
+        if (Price < 0 || Price > 10000)
+            throw new IllegalArgumentException("The price needs to be within 0-10000 (inclusive)");
+
+        this.Price = Price;
+    }
+
     public Component(String Name, String Manufacturer, double Price)
     {
 
