5d0adfcaa94dcb691bda32a14606131a1da8aa72
Matthew Daniel Contaldi <mdcontal@lakeheadu.ca>
2024-02-05 13:45:51 -0500

    question 3.1 complete

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -1,5 +1,10 @@
 import java.util.ArrayList;
 
 public class ShoppingCart {
+    private ArrayList<Component> Items;
 
+    public ShoppingCart()
+    {
+        Items = new ArrayList<Component>();
+    }
 }
